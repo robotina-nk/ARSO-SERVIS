1840cc35260001dd11f9c5ec0a250c1f48c855c6
Nikola Kostic <nikola.kostic@robotina.com>
2017-08-30 09:27:51 +0200

    ok

--- TASKI/WS700/WS700.sh
+++ TASKI/WS700/WS700.sh
@@ -1,1 +1,19 @@
+WS700-UMB 8380.U01 (Europe, USA, Canada)
+ Precipitation Radar
+ Wind Direction
+ Wind Speed
+ Air Temperature
+ Relative Humidity
+ Air Pressure
+ Compass
+ Global Radiation
 
+Pin assignment:
+1 White Supply voltage ground and (SDI12_GND for device version > 41)
+2 Brown Positive supply voltage (through 2.5A fuse where required)*
+3 Green RS485_A (+) or (SDI-12 GND for device version < 42)
+4 Yellow RS485_B (-) or SDI-12 Data Line
+5 Grey External Sensor a (WS100-UMB only: impulse output Uout)
+6 Pink External Sensor b (WS100-UMB only: not connected)
+7 Blue Heating voltage ground
+8 Red Positive heating voltage (through 2.5A fuse where required)*
