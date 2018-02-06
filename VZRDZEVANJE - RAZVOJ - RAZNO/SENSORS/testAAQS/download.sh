d497d9ef4fbc58f9846f0e6a7d3d7ca247333f1f
robotina-nk <nikola.kostic@robotina.com>
2018-02-06 11:54:20 +0100

    ok

--- VZRDZEVANJE - RAZVOJ - RAZNO/SENSORS/testAAQS/download.sh
+++
@@ -1,3 +0,0 @@
-# import textile to "clipboard"
-#curl -i -H "Accept: application/xml" -H "Content-Type: application/xml" -X GET -u nkostic:Kosnik00 http://pivka.arso.sigov.si/redmine/projects/aaqs_location_00402_e402_mobilna_postaja_kombi/wiki/Wiki.xml | xclip -selection c
-curl -i -H "Accept: application/xml" -H "Content-Type: application/xml" -X GET -u nkostic:Kosnik00 http://pivka.arso.sigov.si/redmine/projects/e4000_test_wiki-testna-postaja/wiki/Wiki.xml > textile.xml
--- VZRDZEVANJE - RAZVOJ - RAZNO/SENSORS/testAAQS/upload.sh
+++
@@ -1,1 +0,0 @@
-curl -v -u nkostic:Kosnik00 -H "Content-Type: application/xml" -X PUT http://pivka.arso.sigov.si/redmine/projects/e4000_test_wiki-testna-postaja/wiki/Wiki.xml --data-binary "@textile.xml"
